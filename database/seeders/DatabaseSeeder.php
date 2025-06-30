266e44f5fdf0e91637cedda9c107397f79381afc
Luna Marrano <lunamarrano2@gmail.com>
2025-06-30 18:26:23 -0300

    correccion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             OrderStatusSeeder::class, 
             UsersSeeder::class,        
             VehicleTypeSeeder::class,   
+            //comentario
         ]);
     }
 }
