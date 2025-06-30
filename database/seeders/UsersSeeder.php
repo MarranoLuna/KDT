d2602711eff308c889e455d4711f024bc5f93476
Federico Guigou <fefogs@gmail.com>
2025-06-30 17:07:59 -0300

    Mensaje

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -46,37 +46,6 @@ class UsersSeeder extends Seeder
         foreach ($usuarios as $data) {
             User::factory()->create($data);
         }
-        /*
-        
-        DB::table('users')->insert([
-            [
-                'email' => 'fefogs@gmail.com',
-                'firstname' => "Federico",
-                'lastname' => "Guigou",
-                'password' => "1234",
-                'phone'=> '215474',
-                'birthday' =>'1997-08-15',
-                'email_verified_at' => now(),
-            ],
-            [
-                'email' => 'elisaronconi28@gmail.com',
-                'firstname' => "Elisa",
-                'lastname' => "Ronconi",
-                'password' => "1234",
-                'phone'=> '621245',
-                'birthday' =>'2000-07-15',
-                'email_verified_at' => now(),
-            ],
-            [
-                'email' => 'lunamarrano2@gmail.com',
-                'firstname' => "Luna",
-                'lastname' => "Marrano",
-                'password' => "1234",
-                'phone'=> '541881',
-                'birthday' =>'2003-04-26',
-                'email_verified_at' => now(),
-            ],
-        ]);
-        */
+
     }
 }
