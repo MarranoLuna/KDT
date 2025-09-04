10c67010994f2a51260ef2361d880050f1ba8748
Luna Marrano <lunamarrano2@gmail.com>
2025-09-04 17:09:05 -0300

    controller nuevo

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-//Route::get('/posts', [PostController::class, 'index']);
-//Route::post('/posts', [PostController::class, 'store']);
-//Route::get('/posts/{post}', [PostController::class, 'show']);
-//Route::put('/posts/{post}', [PostController::class, 'update']);
-//Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'destroy']);
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
