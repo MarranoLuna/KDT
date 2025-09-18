13a911bcdb72bf5dabc08c927dbc9a8abb41a3d3
MarranoLuna <lunamarrano2@gmail.com>
2025-09-18 19:12:50 -0300

    rutas de obtener y guardar usuario

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-
 use App\Http\Controllers\RequestController;
+use App\Http\Controllers\UserController;
 
 
 Route::post('/requests', [RequestController::class, 'store']);
@@ -17,3 +17,9 @@ Route::get('/requests/user/{id}', [RequestController::class, 'getUserRequests'])
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Ruta para obtener los datos de un usuario por su ID
+Route::get('/users/{user}', [UserController::class, 'show']);
+
+// Ruta para actualizar los datos de un usuario por su ID
+Route::put('/users/{user}', [UserController::class, 'update']); 
